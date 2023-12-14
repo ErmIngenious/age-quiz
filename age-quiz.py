94a661d47961a198167ad86331f37179873fdf21
Emma-Louise Moses <148122558+ErmIngenious@users.noreply.github.com>
2023-12-14 17:15:26 +0000

    Update age-quiz.py
    code

--- age-quiz.py
+++ age-quiz.py
@@ -1,24 +1,3 @@
-'''
-psuedocode:
-get user to input age and store it in an interger varialble
-if age is under 13 
-out put message "You qualify for the kiddie discount"
-if user is 21
-output message"Congrats on your 21st"
-if age is over 40 
-output the message "Your over the hill"
-if user is 65 or older 
-output message "Enjoy your retirement"
-The oldest someone can be is 100
-if age is greater than 100 
-output message "Sorry your dead"
-
-
-for any other age
-output message "Age is but a qtber'
-
-'''
-
 # ask user age and store in an interger variable
 age = int(input('Age?'))
 
